40f6cf92236665eb3c57b78752dc576215ae3fc7
Javid Sadigli <cavidoffical@gmail.com>
2024-10-19 19:53:44 +0400

    Started a Spring Boot project

---
+++ src/main/java/az/ufaz/stock_predictor/StockPredictorApplication.java
@@ -0,0 +1,13 @@
+package az.ufaz.stock_predictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPredictorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/ufaz/stock_predictor/StockPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ufaz.stock_predictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
