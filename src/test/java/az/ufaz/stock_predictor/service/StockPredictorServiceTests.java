51c18c5debecacd809366352a40e578be9e48953
Javid Sadigli <cavidoffical@gmail.com>
2025-04-06 01:08:29 +0400

    Added First test

--- src/test/java/az/ufaz/stock_predictor/service/StockPredictorServiceTests.java
+++ src/test/java/az/ufaz/stock_predictor/service/StockPredictorServiceTests.java
@@ -1,18 +1,30 @@
 package az.ufaz.stock_predictor.service;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.mapstruct.factory.Mappers;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import az.ufaz.stock_predictor.client.StockPredictorAIClient;
 import az.ufaz.stock_predictor.mapper.StockPredictorMapper;
+import az.ufaz.stock_predictor.model.dto.client.StockPredictorBaseDTO;
+import az.ufaz.stock_predictor.model.dto.client.StockPredictorSimpleStockDTO;
+import az.ufaz.stock_predictor.model.dto.response.BaseResponse;
+import az.ufaz.stock_predictor.model.dto.response.SimpleStockResponse;
+import az.ufaz.stock_predictor.model.enums.StockPredictionSimpleStockInterval;
+import az.ufaz.stock_predictor.service.provider.StockPredictorServiceDataProvider;
 
 @ExtendWith(value = MockitoExtension.class)
 public class StockPredictorServiceTests 
 {
-    private StockPredictorService stockPredictorService; 
+    private StockPredictorService service; 
     private StockPredictorAIClient stockPredictorAIClient; 
     private StockPredictorMapper stockPredictorMapper; 
 
@@ -21,8 +33,35 @@ public class StockPredictorServiceTests
     {
         stockPredictorAIClient = Mockito.mock(StockPredictorAIClient.class);
         stockPredictorMapper = Mappers.getMapper(StockPredictorMapper.class); 
-        stockPredictorService = new StockPredictorService(stockPredictorAIClient, stockPredictorMapper);
+        service = new StockPredictorService(stockPredictorAIClient, stockPredictorMapper);
     }
 
-    
+    @ParameterizedTest
+    @ArgumentsSource(value = StockPredictorServiceDataProvider.StockPredictionProvider.class)
+    @DisplayName(value = "Testing stock prediction when everything is ok")
+    public void givenStockPrediction_WhenEverythingIsOk_ThenReturnStockPredictions(
+        String stockName, 
+        StockPredictionSimpleStockInterval interval, 
+        int duration, 
+        String intervalString,
+        StockPredictorSimpleStockDTO predictionDto, 
+        int status,
+        List<SimpleStockResponse> stockResponseList
+    ){
+        Mockito.when(stockPredictorAIClient.getStockPrediction(stockName, intervalString, duration))
+            .thenReturn(
+                StockPredictorBaseDTO.<StockPredictorSimpleStockDTO>builder()
+                    .data(predictionDto)
+                    .message("Predictions made successfully.")
+                    .success(true)
+                    .status(status)
+                    .build()
+            );
+
+        BaseResponse<List<SimpleStockResponse>> serviceResponse = service.getStockPrediction(stockName, interval, duration);
+
+        Assertions.assertEquals(status, serviceResponse.getStatus());
+        Assertions.assertEquals("Predictions made successfully.", serviceResponse.getMessage());
+        Assertions.assertEquals(stockResponseList, serviceResponse.getData());
+    }
 }
---
+++ src/test/java/az/ufaz/stock_predictor/service/provider/StockPredictorServiceDataProvider.java
@@ -0,0 +1,76 @@
+package az.ufaz.stock_predictor.service.provider;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.springframework.http.HttpStatus;
+
+import az.ufaz.stock_predictor.model.dto.client.StockPredictorSimpleStockDTO;
+import az.ufaz.stock_predictor.model.dto.response.SimpleStockResponse;
+import az.ufaz.stock_predictor.model.enums.StockPredictionSimpleStockInterval;
+
+public class StockPredictorServiceDataProvider 
+{
+    public static class StockPredictionProvider implements ArgumentsProvider
+    {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context)
+        {
+            return Stream.of(
+                Arguments.of(
+                    "AAPL", 
+                    StockPredictionSimpleStockInterval.ONE_HOUR,
+                    5, 
+                    "1h",
+                    StockPredictorSimpleStockDTO.builder()
+                        .prices(
+                            List.of(
+                                193.3159942626953, 
+                                192.48828125,
+                                191.85888671875,
+                                191.50152587890625,
+                                191.41998291015625
+                            )
+                        )
+                        .timestamps(
+                            List.of(
+                                "2025-04-04T20:30:00+00:00",
+                                "2025-04-04T21:30:00+00:00",
+                                "2025-04-04T22:30:00+00:00",
+                                "2025-04-04T23:30:00+00:00",
+                                "2025-04-05T00:30:00+00:00"
+                            )
+                        )
+                        .build(),
+                    HttpStatus.OK.value(),
+                    List.of(
+                        SimpleStockResponse.builder()
+                            .price(193.3159942626953)
+                            .timestamp(LocalDateTime.of(2025, 4, 4, 20, 30, 0))
+                            .build(), 
+                        SimpleStockResponse.builder()
+                           .price(192.48828125)
+                           .timestamp(LocalDateTime.of(2025, 4, 4, 21, 30, 0))
+                           .build(), 
+                        SimpleStockResponse.builder()
+                           .price(191.85888671875)
+                           .timestamp(LocalDateTime.of(2025, 4, 4, 22, 30, 0))
+                            .build(), 
+                        SimpleStockResponse.builder()
+                           .price(191.50152587890625)
+                           .timestamp(LocalDateTime.of(2025, 4, 4, 23, 30, 0))
+                           .build(), 
+                        SimpleStockResponse.builder()
+                           .price(191.41998291015625)
+                           .timestamp(LocalDateTime.of(2025, 4, 5, 0, 30, 0))
+                            .build()
+                    )
+                )
+            );
+        }
+    }
+}
